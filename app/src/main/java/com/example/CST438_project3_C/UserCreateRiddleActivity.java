0d5fe34089abfa9549f9bc72afe91071bf8df794
fjhernan <60201738+fjhernan@users.noreply.github.com>
2021-04-14 13:45:07 -0700

    Added and working on layout for user create riddle.

--- app/src/main/java/com/example/CST438_project3_C/UserCreateRiddleActivity.java
+++ app/src/main/java/com/example/CST438_project3_C/UserCreateRiddleActivity.java
@@ -1,4 +1,30 @@
 package com.example.CST438_project3_C;
 
-public class UserCreateRiddleActivity {
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This activity lets a user create and upload a riddle
+ * @author Francisco Hernandez
+ * @version 1.1
+ * @since 04-14-2021
+ */
+public class UserCreateRiddleActivity extends AppCompatActivity {
+    public static final String USER_CREATE_RIDDLE_ACTIVITY = "UserCreateRiddleActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_create_riddle);
+        //The following is code for when the riddle database is made
+        //String name = savedInstanceState.getString(NAME);
+
+
+
+
+    }
+
+
 }

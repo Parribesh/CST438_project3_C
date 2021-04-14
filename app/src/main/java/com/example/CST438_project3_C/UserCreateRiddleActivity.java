07b8d41c891c949a9f4c0d8fe91a866196276cf4
fjhernan <60201738+fjhernan@users.noreply.github.com>
2021-04-14 16:49:23 -0700

    Fixed check for emptiness. Some weird issues with Log.d and .getText().toString() is going on.

--- app/src/main/java/com/example/CST438_project3_C/UserCreateRiddleActivity.java
+++ app/src/main/java/com/example/CST438_project3_C/UserCreateRiddleActivity.java
@@ -1,6 +1,7 @@
 package com.example.CST438_project3_C;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,6 +10,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.CST438_project3_C.data.User;
+
+import org.w3c.dom.Text;
 
 /**
  * This activity lets a user create and upload a riddle
@@ -21,34 +24,48 @@ public class UserCreateRiddleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Log.d doesn't work for some reason.
+        //Log.d(USER_CREATE_RIDDLE_ACTIVITY, "onCreate Called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_create_riddle);
-        //The following is code for when the riddle database is made
-        //String name = savedInstanceState.getString(NAME);
+
         EditText eRiddle = findViewById(R.id.user_create_riddle_riddle);
         EditText eAnswer = findViewById(R.id.user_create_riddle_answer);
-        String riddle = eRiddle.getText().toString();
-        String answer = eAnswer.getText().toString();
+        //for some reason these variables don't actually save data
+        //String riddle = eRiddle.getText().toString();
+        //String answer = eAnswer.getText().toString();
         Button upload = findViewById(R.id.user_create_riddle_button);
         upload.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 String msg = "";
-                if(riddle.isEmpty()){
+                Toast t;
+                boolean check = false;
+                if(eRiddle.getText().toString().isEmpty()){
                     msg += "You can't upload a blank riddle!\n";
+                    check = true;
+                } else{
+                    msg += "You made a riddle!\n";
                 }
-                if(answer.isEmpty()){
-                    msg += "You can't upload a riddle without an answer!\n";
+                if(eAnswer.getText().toString().isEmpty()){
+                    msg += "You can't upload a riddle without an answer!";
+                    check = true;
+                } else{
+                    msg += "You have an answer!";
                 }
-                if(!(msg.isEmpty())){
-                    Toast t = Toast.makeText(UserCreateRiddleActivity.this, msg,
+                if(check == false){
+                    t = Toast.makeText(UserCreateRiddleActivity.this, msg,
                             Toast.LENGTH_LONG);
                     t.show();
+                } else{
+                    t = Toast.makeText(UserCreateRiddleActivity.this, msg,
+                            Toast.LENGTH_LONG);
+                    t.show();
+                    /*
+                      Code for adding to database will go here
+                     */
                 }
             }
         });
-
     }
-
-
 }

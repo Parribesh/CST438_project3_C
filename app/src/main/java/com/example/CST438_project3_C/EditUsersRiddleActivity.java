36ff0aee6259c9d7dcca7fc8302f732c4fb5bbb6
fjhernan <60201738+fjhernan@users.noreply.github.com>
2021-04-15 13:11:32 -0700

    Creation of activity.

---
+++ app/src/main/java/com/example/CST438_project3_C/EditUsersRiddleActivity.java
@@ -0,0 +1,16 @@
+package com.example.CST438_project3_C;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This acticity lets users edit riddles they created.
+ */
+public class EditUsersRiddleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

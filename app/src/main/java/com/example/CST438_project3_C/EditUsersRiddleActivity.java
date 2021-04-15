a452e035752c7916265c2bb527f092547727c195
Francisco Hernandez <60201738+fjhernan@users.noreply.github.com>
2021-04-15 13:16:48 -0700

    Just doing this for the homework

--- app/src/main/java/com/example/CST438_project3_C/EditUsersRiddleActivity.java
+++ app/src/main/java/com/example/CST438_project3_C/EditUsersRiddleActivity.java
@@ -1,6 +1,7 @@
 package com.example.CST438_project3_C;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
  * This acticity lets users edit riddles they created.
  */
 public class EditUsersRiddleActivity extends AppCompatActivity {
+    public static final String EDIT_USERS_RIDDLE_ACTIVITY = "EditUsersRiddleActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(EDIT_USERS_RIDDLE_ACTIVITY, "onCreate called");
         super.onCreate(savedInstanceState);
     }
 }

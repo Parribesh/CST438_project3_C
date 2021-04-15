018c82dc8ef9cf96038eed2b2bf5ad704d7690a0
enyaw15 <enyaw0215@gmail.com>
2021-04-15 10:23:08 -1000

    updated firebase to match project package name

--- app/src/main/java/com/example/CST438_project3_C/MainActivity.java
+++ app/src/main/java/com/example/CST438_project3_C/MainActivity.java
@@ -14,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        testFirebase();
+    }
+
+    private void testFirebase(){
+        / Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+    // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 }

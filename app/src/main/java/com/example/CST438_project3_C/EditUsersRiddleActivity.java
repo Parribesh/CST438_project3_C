88f682f48cfa6e23d5b3fcc915b7caedd1d709b4
Francisco Hernandez <60201738+fjhernan@users.noreply.github.com>
2021-04-20 14:16:36 -0700

    Added a layout.

--- app/src/main/java/com/example/CST438_project3_C/EditUsersRiddleActivity.java
+++ app/src/main/java/com/example/CST438_project3_C/EditUsersRiddleActivity.java
@@ -15,6 +15,7 @@ public class EditUsersRiddleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(EDIT_USERS_RIDDLE_ACTIVITY, "onCreate called");
         super.onCreate(savedInstanceState);
-        //random text stuff
+        setContentView(R.layout.activity_edit_user_riddle_activity);
+
     }
 }

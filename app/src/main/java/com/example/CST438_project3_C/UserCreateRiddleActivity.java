222da451e76773d61c8470003b8abce4b836301b
Francisco Hernandez <60201738+fjhernan@users.noreply.github.com>
2021-04-27 13:59:46 -0700

    Figured out how to update database.

--- app/src/main/java/com/example/CST438_project3_C/UserCreateRiddleActivity.java
+++ app/src/main/java/com/example/CST438_project3_C/UserCreateRiddleActivity.java
@@ -7,11 +7,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.CST438_project3_C.data.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This activity lets a user create and upload a riddle
@@ -21,34 +30,42 @@ import org.w3c.dom.Text;
  */
 public class UserCreateRiddleActivity extends AppCompatActivity {
     public static final String USER_CREATE_RIDDLE_ACTIVITY = "UserCreateRiddleActivity";
+    private String userKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Log.d doesn't work for some reason.
-        //Log.d(USER_CREATE_RIDDLE_ACTIVITY, "onCreate Called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_create_riddle);
 
         EditText eRiddle = findViewById(R.id.user_create_riddle_riddle);
         EditText eAnswer = findViewById(R.id.user_create_riddle_answer);
-        //for some reason these variables don't actually save data
-        //String riddle = eRiddle.getText().toString();
-        //String answer = eAnswer.getText().toString();
         Button upload = findViewById(R.id.user_create_riddle_button);
         upload.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Toast t;
                 boolean checkR = checkRiddles(eRiddle.getText().toString());
                 boolean checkS = checkSolution(eAnswer.getText().toString());
                 if(!checkR && !checkS){
-                    //If everything is good
-                    t = Toast.makeText(UserCreateRiddleActivity.this, ":)",
-                            Toast.LENGTH_LONG);
-                    t.show();
-                    /*
-                      Code for adding to database will go here
-                     */
+                    DatabaseReference dr = FirebaseDatabase.getInstance().getReference().child("users");
+                    dr.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                                if(dataSnapshot.toString().contains("fjhernan@gmail.com")) {
+                                    userKey = dataSnapshot.getKey();
+                                    DatabaseReference d = dr.child(dataSnapshot.getKey());
+                                    Map<String, Object> update = new HashMap<>();
+                                    update.put("Riddle","Testing");
+                                    d.updateChildren(update);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 } else{
                     //If everything is not good
                     String msg = "";
@@ -58,7 +75,7 @@ public class UserCreateRiddleActivity extends AppCompatActivity {
                     if(checkS){
                         msg += "You can't have a riddle without a solution!";
                     }
-                    t = Toast.makeText(UserCreateRiddleActivity.this, msg,
+                    Toast t = Toast.makeText(UserCreateRiddleActivity.this, msg,
                             Toast.LENGTH_LONG);
                     t.show();
                 }

fc4f758657a74672b96f28b31aeb3142d5af07a4
Francisco Hernandez <60201738+fjhernan@users.noreply.github.com>
2021-05-11 15:13:52 -0700

    Riddles can now be deleted from database.

--- app/src/main/java/com/example/CST438_project3_C/UserDeleteRiddleActivity.java
+++ app/src/main/java/com/example/CST438_project3_C/UserDeleteRiddleActivity.java
@@ -1,5 +1,6 @@
 package com.example.CST438_project3_C;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,19 +11,25 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.CST438_project3_C.data.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class UserDeleteRiddleActivity extends AppCompatActivity {
-    ArrayList<String> arrayList = new ArrayList<>();
+    ArrayList<String> riddleList = new ArrayList<>();
+    ArrayList<String> indexList = new ArrayList<>();
     ListView listView;
     ArrayAdapter<String> arrayAdapter;
 
@@ -31,42 +38,51 @@ public class UserDeleteRiddleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_delete_riddles);
         listView = (ListView) findViewById(R.id.user_delete_list_view);
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, riddleList);
         listView.setAdapter(arrayAdapter);
         String loggedUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference dr = FirebaseDatabase.getInstance().getReference()
                 .child("riddles").child(loggedUser);
-
-
-        dr.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        dr.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(task.isSuccessful()){
-                    Toast p;
-                    DataSnapshot snapshot = task.getResult();
-                    for(DataSnapshot snap : snapshot.getChildren()){
-                        String data = snap.getValue().toString();
-                        arrayList.add(data);
-                        arrayAdapter.notifyDataSetChanged();
-                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                                Toast k = Toast.makeText(UserDeleteRiddleActivity.this, "hello world",
-                                        Toast.LENGTH_LONG);
-                                k.show();
-                            }
-                        });
-                    }
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot snap : snapshot.getChildren()){
+                    String data = snap.getValue().toString();
+                    riddleList.add(data);
+                    indexList.add(snap.getKey());
+                    arrayAdapter.notifyDataSetChanged();
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int j, long l) {
+                            AlertDialog.Builder alert = new AlertDialog.Builder(UserDeleteRiddleActivity.this);
+                            alert.setMessage("Are you sure you want to delete this riddle?");
+                            alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    //do nothing
+                                    dr.child(indexList.get(j)).removeValue();
+                                    indexList.remove(j);
+                                    finish();
+                                    startActivity(getIntent());
+                                }
+                            });
+                            alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    //do nothing
+                                }
+                            });
+                            alert.show();
+                        }
+                    });
                 }
             }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
-/*
-        if(task.isSuccessful()){
-            String value = task.getResult().getValue().toString();
-            String edited = value.substring(1, value.length()-1);
-            String [] strings = edited.split("=", 2);
-        }
-  */
 
         Button returnButton = findViewById(R.id.user_delete_return_button);
         returnButton.setOnClickListener(new View.OnClickListener() {

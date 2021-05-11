050b3a2396dc16d2ad08f507d4ae764eb600baba
Francisco Hernandez <60201738+fjhernan@users.noreply.github.com>
2021-05-11 14:33:42 -0700

    User riddles are shown and are clickable. Need to add delete functionality.

--- app/src/main/java/com/example/CST438_project3_C/UserDeleteRiddleActivity.java
+++ app/src/main/java/com/example/CST438_project3_C/UserDeleteRiddleActivity.java
@@ -3,7 +3,10 @@ package com.example.CST438_project3_C;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,16 +19,24 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class UserDeleteRiddleActivity extends AppCompatActivity {
+    ArrayList<String> arrayList = new ArrayList<>();
+    ListView listView;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_delete_riddles);
-
+        listView = (ListView) findViewById(R.id.user_delete_list_view);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
         String loggedUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference dr = FirebaseDatabase.getInstance().getReference()
                 .child("riddles").child(loggedUser);
+
 
         dr.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
@@ -35,9 +46,16 @@ public class UserDeleteRiddleActivity extends AppCompatActivity {
                     DataSnapshot snapshot = task.getResult();
                     for(DataSnapshot snap : snapshot.getChildren()){
                         String data = snap.getValue().toString();
-                        p = Toast.makeText(UserDeleteRiddleActivity.this, data
-                                , Toast.LENGTH_LONG);
-                        p.show();
+                        arrayList.add(data);
+                        arrayAdapter.notifyDataSetChanged();
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                Toast k = Toast.makeText(UserDeleteRiddleActivity.this, "hello world",
+                                        Toast.LENGTH_LONG);
+                                k.show();
+                            }
+                        });
                     }
                 }
             }
